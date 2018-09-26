bdfe4e95d08a6de4043887ee2f67afbbccac0a36
nicolaslema2710 <32146297+nicolaslema2710@users.noreply.github.com>
2018-09-25 23:27:48 -0300

    Sessions fix 1

--- sessions/procesar_session.php
+++ sessions/procesar_session.php
@@ -1,10 +1,21 @@
-<?php
+<?php session_start();
 require_once('../backend/classes/user.class.php');
 require_once('../backend/classes/data.class.php');
 
 $inputUser = $_POST['usuario'];
 $inputPassword = $_POST['contraseña'];
+$checklogin - User::userLogin($inputUser,$inputPassword);
 
-var_dump(User::userLogin($inputUser, $inputPassword));
+if(gettype($checklogin) == "object"){
 
+    $_SESSION['ingreso'] = "object";
+    header("Location: /index.php");
+    die();
 
+}else if($checklogin == "wrongPassword"){
+    echo "error1";
+    header("Location: ../index.php");
+}else{
+    echo "error2";
+    header("Location: ../index.php");
+}
